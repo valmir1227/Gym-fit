2a04cb0d0b33195fe094bf3060f572854eb6e40f
Valmir Almeida <almeidavalmir76@gmail.com>
2023-01-04 00:53:19 -0300

    Menu finished

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -38,10 +38,46 @@ export const Head = styled.header`
   }
 
   @media ${device.tablet} {
-    padding: 1rem 3rem;
+    padding: 2rem 3rem;
   }
 
   @media ${device.laptop} {
-    padding: 1rem 4rem;
+    padding: 2rem 4rem;
+  }
+`;
+
+export const Mobile = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem 1rem;
+  position: absolute;
+
+  div {
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: flex-end;
+    nav {
+      list-style: none;
+      background: rgba(146, 88, 249, 0.38);
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+      backdrop-filter: blur(5px);
+      border: 1px solid rgba(146, 88, 249, 1);
+      text-align: center;
+      padding: 2rem 1rem;
+      margin-top: 0.5rem;
+      border-radius: 5px;
+      z-index: 99;
+
+      li {
+        padding: 0.5rem 0;
+        text-transform: uppercase;
+        font-weight: 500;
+        color: #fff;
+      }
+    }
+    svg {
+      cursor: pointer;
+    }
   }
 `;
